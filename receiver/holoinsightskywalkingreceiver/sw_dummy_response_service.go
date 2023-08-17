8450c8095a836dad81fecf05f451f73303621a6e
sw1136562366 <sw1136562366@gmail.com>
2023-08-17 14:43:17 +0800

    feat: trace dynamic configuration

--- receiver/holoinsightskywalkingreceiver/sw_dummy_response_service.go
+++ receiver/holoinsightskywalkingreceiver/sw_dummy_response_service.go
@@ -16,6 +16,10 @@ package holoinsightskywalkingreceiver // import "github.com/open-telemetry/opent
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/traas-stack/holoinsight-collector/internal/utils"
 	"go.opentelemetry.io/collector/config/confighttp"
 	"go.uber.org/zap"
 	v3c "skywalking.apache.org/repo/goapi/collect/agent/configuration/v3"
@@ -24,6 +28,14 @@ import (
 	agent "skywalking.apache.org/repo/goapi/collect/language/agent/v3"
 	profile "skywalking.apache.org/repo/goapi/collect/language/profile/v3"
 	management "skywalking.apache.org/repo/goapi/collect/management/v3"
+	"strings"
+)
+
+const (
+	GatewayAgentConfigURL             = "/internal/api/gateway/agent/configuration/query"
+	GatewayAgentConfigTenantParam     = "tenant"
+	GatewayAgentConfigServiceParam    = "service"
+	GatewayAgentConfigExtendInfoParam = "extendInfo"
 )
 
 type dummyReportService struct {
@@ -42,10 +54,14 @@ type dummyReportService struct {
 type AgentConfiguration struct {
 	Tenant        string
 	Service       string
-	AppID         string
-	EnvID         string
 	Configuration map[string]string
 	UUID          string
+}
+
+type AgentConfigurationRequest struct {
+	Tenant     string `json:"tenant"`
+	Service    string `json:"service"`
+	ExtendInfo string `json:"extendInfo"`
 }
 
 // for sw InstanceProperties
@@ -65,6 +81,68 @@ func (d *dummyReportService) Collect(_ context.Context, jvm *agent.JVMMetricColl
 
 // for sw agent cds
 func (d *dummyReportService) FetchConfigurations(ctx context.Context, req *v3c.ConfigurationSyncRequest) (*common.Commands, error) {
+	if d.GatewayHTTPSettings.Endpoint == "" {
+		d.logger.Error("[fetchConfigurations] Holoinsight server http endpoint not set! ")
+	}
+	tenantValue := ctx.Value(Tenant)
+	if tenantValue == nil {
+		d.logger.Error("[fetchConfigurations] tenant cannot be empty!")
+		return &common.Commands{}, nil
+	}
+	tenant := tenantValue.(string)
+	service := req.GetService()
+
+	var extendInfo string
+	extendValue := ctx.Value(ExtendTags)
+	if extendValue != nil {
+		extendMap := extendValue.(map[string]string)
+		extendMap["type"] = "skywalking"
+		jsonStr, _ := json.Marshal(extendMap)
+		extendInfo = string(jsonStr)
+	}
+
+	url := d.GatewayHTTPSettings.Endpoint
+	if !strings.HasPrefix(d.GatewayHTTPSettings.Endpoint, "http://") {
+		url = "http://" + d.GatewayHTTPSettings.Endpoint
+	}
+	request := &AgentConfigurationRequest{
+		Tenant:     tenant,
+		Service:    service,
+		ExtendInfo: extendInfo,
+	}
+	requestBody, _ := json.Marshal(request)
+	response, err := utils.HTTPPost(url+GatewayAgentConfigURL, string(requestBody))
+	if err != nil {
+		d.logger.Error("[fetchConfigurations] Get agent configurations from Holoinsight server error: ", zap.Error(err))
+		return &common.Commands{}, nil
+	}
+	if len(response) == 0 {
+		d.logger.Debug(fmt.Sprintf("[fetchConfigurations] tenant: %s, service: %s , extendInfo: %s, configurations is null!",
+			tenant, service, extendInfo))
+		return &common.Commands{}, nil
+	}
+
+	agentConfiguration := &AgentConfiguration{}
+	err = json.Unmarshal(response, agentConfiguration)
+	if err != nil {
+		d.logger.Error("[fetchConfigurations] Agent configurations unmarshal error: ", zap.Error(err))
+	}
+
+	if len(response) != 0 && agentConfiguration.UUID != req.GetUuid() {
+		configList := make([]*common.KeyStringValuePair, 0, 8)
+		configList = append(configList, &common.KeyStringValuePair{Key: "UUID", Value: agentConfiguration.UUID})
+		configList = append(configList, &common.KeyStringValuePair{Key: "SerialNumber", Value: uuid.New().String()})
+
+		for key, value := range agentConfiguration.Configuration {
+			configList = append(configList, &common.KeyStringValuePair{Key: key, Value: value})
+		}
+
+		c := &common.Command{Command: "ConfigurationDiscoveryCommand", Args: configList}
+		d.logger.Info(fmt.Sprintf("[fetchConfigurations] tenant: %s, service: %s, extendInfo: %s, config: %s",
+			agentConfiguration.Tenant, agentConfiguration.Service, extendInfo, agentConfiguration.Configuration))
+
+		return &common.Commands{Commands: []*common.Command{c}}, nil
+	}
 	return &common.Commands{}, nil
 }
 
